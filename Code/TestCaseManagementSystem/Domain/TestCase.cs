9027656a9e1521c54221e6cc970127427aba9811
Marco Prescher <m.prescher@gantner-instruments.com>
2023-03-15 18:54:18 +0100

    Added domain class TestCase.cs

---
+++ Code/TestCaseManagementSystem/Domain/TestCase.cs
@@ -0,0 +1,34 @@
+﻿using Domain.enums;
+using Domain.ids;
+
+namespace Domain;
+
+public class TestCase
+{
+    private long Id { get; }
+    private TestCaseId DomainId { get; }
+    
+    private string ShortDescription { get; set; }
+    private string LongDescription { get; set; }
+    private string AuthorDescription { get; }
+    private DateTime CreateDate { get; }
+    private Priority Priority { get; set; }
+    private string ReferenceLink { get; set; }
+
+    public TestCase(long id, TestCaseId domainId, string shortDescription, string longDescription, string authorDescription, DateTime createDate, Priority priority, string referenceLink)
+    {
+        Id = id;
+        DomainId = domainId;
+        ShortDescription = shortDescription;
+        LongDescription = longDescription;
+        AuthorDescription = authorDescription;
+        CreateDate = createDate;
+        Priority = priority;
+        ReferenceLink = referenceLink;
+    }
+
+    public override string ToString()
+    {
+        return DomainId.ToString();
+    }
+}
