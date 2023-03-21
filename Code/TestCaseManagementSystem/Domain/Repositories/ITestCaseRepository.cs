4e24e8e01b57edc91e2fbd3a09aef42b578b96fc
Marco Prescher <m.prescher@gantner-instruments.com>
2023-03-21 17:38:35 +0100

    added TestCaseRepository.cs and ITestCaseRepository.cs

---
+++ Code/TestCaseManagementSystem/Domain/Repositories/ITestCaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Repositories;
+
+public interface ITestCaseRepository
+{
+    Task<TestCase> GetById(int id);
+    Task<List<TestCase>> GetAll();
+    Task Add(TestCase testCase);
+    Task Update(TestCase testCase);
+    Task Remove(TestCase testCase);
+}
---
+++ Code/TestCaseManagementSystem/Infrastructure/TestCaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Domain;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class TestCaseRepository : ITestCaseRepository
+{
+    private readonly EFContext _context;
+
+    public TestCaseRepository(EFContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<TestCase> GetById(int id)
+    {
+        var testCase = await _context.TestCases.FirstOrDefaultAsync(testCase => testCase.Id == id);
+
+        if (testCase == null)
+        {
+            throw new ArgumentNullException(null, "Test case is null: " + id);
+        }
+        
+        return testCase;
+    }
+
+    public async Task<List<TestCase>> GetAll()
+    {
+        return await _context.TestCases.ToListAsync();
+    }
+
+    public async Task Add(TestCase testCase)
+    {
+        await _context.TestCases.AddAsync(testCase);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(TestCase testCase)
+    {
+        _context.TestCases.Update(testCase);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Remove(TestCase testCase)
+    {
+        _context.TestCases.Remove(testCase);
+        await _context.SaveChangesAsync();
+    }
+}
