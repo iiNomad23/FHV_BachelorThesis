726d72e67f8a532c532c4e7ee25a32cd361f8dee
Marco Prescher <m.prescher@gantner-instruments.com>
2023-03-21 17:26:58 +0100

    Added EFContext.cs Added connection string in appsettings.json

---
+++ Code/TestCaseManagementSystem/Infrastructure/EFContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class EFContext : DbContext
+{
+    public DbSet<TestCase> TestCases { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DDDSample;Trusted_Connection=True;MultipleActiveResultSets=true;");
+    }
+}
