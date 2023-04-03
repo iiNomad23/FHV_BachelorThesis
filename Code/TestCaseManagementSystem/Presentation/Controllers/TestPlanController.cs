2fcea86f9efedd4919dea6976df983f9e537fd00
Marco Prescher <m.prescher@gantner-instruments.com>
2023-04-03 16:20:51 +0200

    Added TestPlanController.cs

---
+++ Code/TestCaseManagementSystem/Presentation/Controllers/TestPlanController.cs
@@ -0,0 +1,31 @@
+﻿using Application.api;
+using Application.dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("api/testplan")]
+public class TestPlanController : ControllerBase
+{
+    private readonly ITestPlanService _testPlanService;
+
+    public TestPlanController(ITestPlanService testPlanService)
+    {
+        _testPlanService = testPlanService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TestPlanDTO>> GetTestPlanById(int id)
+    {
+        var testCaseDTO = await _testPlanService.GetById(id);
+        return Ok(testCaseDTO);
+    }
+    
+    [HttpPost("create")]
+    public async Task<ActionResult<TestCaseDTO>> CreateTestPlan(TestPlanDTO testPlanDTO)
+    {
+        await _testPlanService.Add(testPlanDTO);
+        return Ok(testPlanDTO);
+    }
+}
