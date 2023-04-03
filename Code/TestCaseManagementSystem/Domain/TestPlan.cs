6a46567799fa6561c14cd948c54b3aa1705534b6
Marco Prescher <m.prescher@gantner-instruments.com>
2023-04-03 16:19:38 +0200

    Added TestPlan domain class

---
+++ Code/TestCaseManagementSystem/Domain/TestPlan.cs
@@ -0,0 +1,36 @@
+﻿namespace Domain;
+
+public class TestPlan
+{
+    public long Id { get; }
+    public string DomainId { get; }
+
+    public string ShortDescription { get; set; }
+    public string LongDescription { get; set; }
+    public DateTime CreateDate { get; }
+    public string ReferenceLink { get; set; }
+
+    public TestPlan(string domainId, string shortDescription, string longDescription, DateTime createDate, string referenceLink)
+    {
+        DomainId = domainId;
+        ShortDescription = shortDescription;
+        LongDescription = longDescription;
+        CreateDate = createDate;
+        ReferenceLink = referenceLink;
+    }
+
+    public TestPlan(long id, string domainId, string shortDescription, string longDescription, DateTime createDate, string referenceLink)
+    {
+        Id = id;
+        DomainId = domainId;
+        ShortDescription = shortDescription;
+        LongDescription = longDescription;
+        CreateDate = createDate;
+        ReferenceLink = referenceLink;
+    }
+
+    public override string ToString()
+    {
+        return DomainId;
+    }
+}
