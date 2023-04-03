792a8dda018e5dcaedda077161f320b468ae5eab
Marco Prescher <m.prescher@gantner-instruments.com>
2023-04-03 17:29:44 +0200

    Added TestImplementationController.cs

---
+++ Code/TestCaseManagementSystem/Presentation/Controllers/TestImplementationController.cs
@@ -0,0 +1,31 @@
+﻿using Application.api;
+using Application.dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("api/testImplementation")]
+public class TestImplementationController : ControllerBase
+{
+    private readonly ITestImplementationService _testImplementationService;
+
+    public TestImplementationController(ITestImplementationService testImplementationService)
+    {
+        _testImplementationService = testImplementationService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TestImplementationDTO>> GetTestImplementationById(int id)
+    {
+        var testImplementationDTO = await _testImplementationService.GetById(id);
+        return Ok(testImplementationDTO);
+    }
+    
+    [HttpPost("create")]
+    public async Task<ActionResult<TestImplementationDTO>> CreateTestImplementation(TestImplementationDTO testImplementationDTO)
+    {
+        await _testImplementationService.Add(testImplementationDTO);
+        return Ok(testImplementationDTO);
+    }
+}
